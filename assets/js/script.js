abe82f091baf73d1f1e629c57096f5f469935b15
Blake Torrey <btorrey1998@gmail.com>
2024-09-09 22:25:59 -0600

    added base DOM manipulation for tasks, will require more work

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,18 +40,72 @@ taskInfoSubmit.addEventListener('click', function (event) {
     }
 })
 
+currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'));
+
+function buildTasks () {
+
+    
+    if (!currentTasks) {
+        currentTasks = []
+    }
 
 
 
+    const taskName = currentTasks.taskName;
+    const description = currentTasks.description;
+    const priority = currentTasks.priority;
 
-//     const taskSubmission = {
-//         taskNameInput: taskNameInput.value,
-//         taskDescriptionInput: taskDescriptionInput.value,
-//         taskPriorityInput: taskPriority_input.value
-//     };
-//     // Convert the object to a string and store it
-//     localStorage.setItem('taskSubmission', JSON.stringify(taskSubmission));
 
-// });
+    //need to delete the div with class squareBox in HTML.index
+    const taskSquare = document.createElement('div');
+    const divCreate1 = document.createElement('div');
+    const divCreate2 = document.createElement('div');
+    const headercreate = document.createElement('h5');
+    const paragraphCreate = document.createElement('p');
+    const buttonCreate1 = document.createElement('button');
+    const buttonCreate2 = document.createElement('button');
+    const PriorityCard = document.querySelector(`#${priority}`);
+
+
+    
+    PriorityCard.appendChild(taskSquare);
+    taskSquare.setAttribute('class', 'squareBox');
+    taskSquare.appendChild(divCreate1);
+    divCreate1.setAttribute('class', 'card');
+    divCreate1.appendChild(divCreate2);
+    divCreate2.setAttribute('class', 'card-body');
+    divCreate2.appendChild(headercreate);
+    headercreate.setAttribute('class', 'card-title');
+    headercreate.appendChild(buttonCreate);
+    divCreate2.appendChild(paragraphCreate);
+    paragraphCreate.setAttribute('class', 'card-text');
+    divCreate2.appendChild(buttonCreate1);
+    buttonCreate1.setAttribute('class', 'btn-close text-reset')
+    divCreate2.appendChild(buttonCreate2);
+    buttonCreate2.setAttribute('class', 'btn')
+    headercreate.textContent = taskName;
+    paragraphCreate.textContent = description;
+}
+
+
+
+function sortTasks () {
+    // set the current tasks array to the locally stored tasks
+    currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'))
+
+
+
+    // currentTasks.priority.forEach(buildTasks);
+
+
+
+    // look into using a map for just the priority value to sort by priority.
+    for (let i = 0; i < currentTasks.length; i++) {
+        buildTasks(currentTasks[i]);
+        }
+        
+    }
+
+window.addEventListener("load", sortTasks(currentTasks));
 
 
