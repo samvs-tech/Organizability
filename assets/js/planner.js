436a038b30fa3e8b0af548b6e7bc4c24e4cb9172
morganjtroper2 <morganjtroper@gmail.com>
2024-09-12 20:37:38 -0700

    Decrement function
    Added left arrow decrement function

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -28,7 +28,25 @@ function incrementDate() {
     window.location.reload();
 }
 
+function decrementDate() {
+    let storedDate = localStorage.getItem('currentDate');
+    let currentDate;
+
+    if (storedDate === null) {
+        currentDate = new Date();
+    } else {
+        currentDate = new Date(storedDate);
+    }
+
+    currentDate.setDate(currentDate.getDate() - 1);
+
+    localStorage.setItem('currentDate', currentDate.toISOString());
+
+    window.location.reload();
+}
+
 document.getElementById('rightArrow').addEventListener('click', incrementDate);
+document.getElementById('leftArrow').addEventListener('click', decrementDate);
 
 window.onload = function() {
     displayCurrentDate();
