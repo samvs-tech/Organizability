f23486a7dec2a6031937e2f5fc3d9411e95cdf9f
Blake Torrey <btorrey1998@gmail.com>
2024-09-13 18:02:41 -0600

    added the javascript for a working remove button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,11 @@ const taskDescriptionInput = document.querySelector('#taskDescriptionInput');
 const taskPriorityInput = document.querySelector('#taskPriority_input');
 const taskInfoSubmit = document.querySelector('#taskInfoSubmit');
 
-function TaskEntry(taskName, description, priority) {
+function TaskEntry(taskName, description, priority, id) {
     this.taskName = taskName;
     this.description = description;
     this.priority = priority;
+    this.id = id;
 }
 
 
@@ -22,11 +23,13 @@ taskInfoSubmit.addEventListener('click', function (event) {
 
     // if either the name or description field are not filled in,
     // alerts the following message
-    if (!taskName || !descriptionInput ) {
+    if (!taskName || !descriptionInput) {
         alert("Please fill out all fields")
     } else {
         // creates a new object with name Entry
-        const Entry = new TaskEntry(taskName, descriptionInput, priorityInput);
+        const id = new Date().getTime();
+
+        const Entry = new TaskEntry(taskName, descriptionInput, priorityInput, id);
         // gets the current array of objects from the local storage
         currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'));
         // if there aren't any current tasks, creates a new blank array.
@@ -37,16 +40,18 @@ taskInfoSubmit.addEventListener('click', function (event) {
         currentTasks.push(Entry);
         // stores the updated list of tasks back in the local storage.
         localStorage.setItem('TaskSubmissions', JSON.stringify(currentTasks));
+        location.reload();
     }
 })
 
 currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'));
 
-function buildTasks (currentTask) {
+function buildTasks(currentTask) {
 
     const taskName = currentTask.taskName;
     const description = currentTask.description;
     const priority = currentTask.priority;
+    
 
     // these variables create the element when used
     const divCreate1 = document.createElement('div');
@@ -59,16 +64,16 @@ function buildTasks (currentTask) {
     // selects which section the task will be put in depending on the value of 
     // its input priority.
     const PriorityCard = document.querySelector(`#${priority}`);
-  
+
 
     // creates the first div element underneadth the squarebox
     // based on the priority input for the task.
     PriorityCard.children[0].appendChild(divCreate1);
     // gives the created div the card class
     divCreate1.setAttribute('class', 'card');
-    
+    divCreate1.setAttribute('data-id', currentTask.id);
     // creates another child div 
-     // gives the class 'card body'
+    // gives the class 'card body'
     divCreate1.appendChild(divCreate2);
     divCreate2.setAttribute('class', 'card-body');
 
@@ -88,7 +93,8 @@ function buildTasks (currentTask) {
     // this will eventually be made into the mark complete button
     // and will remove the task from the page
     divCreate2.appendChild(buttonCreate1);
-    buttonCreate1.setAttribute('class', 'btn-close text-reset')
+    buttonCreate1.setAttribute('class', `btn btn-close text-reset complete`);
+
 
     // this is another button created undernead the card-body div
     // it only has the class of button, but the plan is to make it an edit
@@ -100,26 +106,82 @@ function buildTasks (currentTask) {
     // as the input entered into the object.
     headercreate.textContent = taskName;
     paragraphCreate.textContent = description;
+
 }
 
 
+function completeTaskListener() {
+    // all button with the complete class are grouped in a list
+    let completeButton = document.querySelectorAll(`.btn.btn-close.text-reset.complete`);
+    // for each button, create a listener for any click, if clicked,
+    // run the mark complete function
 
+    completeButton.forEach(button => {
 
-function sortTasks () {
+        button.addEventListener('click', event => markComplete(event)
+
+        )
+    });
+}
+
+function markComplete(event) {
+    // grab the array of objects from the local storage
+    currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'))
+    // grab the nearest card class by the button interacted wtih
+    const task = event.currentTarget.closest('.card');
+    
+    // when the element exists, remove the task from the array
+    if (task) {
+        // grab the unique data id from the  targeted task
+        const taskID = task.getAttribute('data-id');
+        
+        // create an empty array to push the objects we want remaining into
+        const updatedTasks = [];
+        // for loop, iterate over each object in the array, if the unique id does not 
+        // match with the targeted id, we want to keep it and push it into our updated
+        // array, the only one left out should be the targeted object.
+  
+
+        for (let i = 0; i < currentTasks.length; i++) {
+            if (currentTasks[i].id !== parseInt(taskID)) {
+                console.log(parseInt(taskID));
+                console.log(currentTasks[i].id);
+                
+                updatedTasks.push(currentTasks[i]);
+                
+            }
+        }
+        // currentTasks.id is a number and taskID is a string
+        // converted currentTasks.id into a string so they are comparable.
+        // it is keeping every object and I don't know why
+        console.log(updatedTasks);
+
+        //update the storage with our new array.
+        localStorage.setItem('TaskSubmissions', JSON.stringify(updatedTasks));
+        //recreate the cards
+        location.reload();
+
+    }
+}
+
+function sortTasks() {
     currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'))
 
     // this for loop causes the buildtasks to iterate over each object in the
     // currentTasks array. It will sort and build out each card for its 
     // corresponding task.
     for (let i = 0; i < currentTasks.length; i++) {
-        buildTasks(currentTasks[i]);
-        }
+        buildTasks(currentTasks[i], i);
     }
+
+    completeTaskListener();
+}
 
 
 
 // when the page finishes loading, this calls the sortTasks fuction and builds
 // the cards for each task.
 window.addEventListener("load", sortTasks(currentTasks));
+
 
 
