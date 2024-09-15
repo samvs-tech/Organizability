d22dc604037d79c897673d84bbffecb87c8ccb88
Blake Torrey <btorrey1998@gmail.com>
2024-09-15 15:01:48 -0600

    Added the ability to edit and move priority of cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const taskNameInput = document.querySelector('#taskNameInput');
 const taskDescriptionInput = document.querySelector('#taskDescriptionInput');
 const taskPriorityInput = document.querySelector('#taskPriority_input');
 const taskInfoSubmit = document.querySelector('#taskInfoSubmit');
+const modalSave = document.getElementById('modalSave');
+
 
 function TaskEntry(taskName, description, priority, id) {
     this.taskName = taskName;
@@ -10,6 +12,7 @@ function TaskEntry(taskName, description, priority, id) {
     this.priority = priority;
     this.id = id;
 }
+
 
 
 // Create an event listener for the submit button 
@@ -48,10 +51,11 @@ currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'));
 
 function buildTasks(currentTask) {
 
+
     const taskName = currentTask.taskName;
     const description = currentTask.description;
     const priority = currentTask.priority;
-    
+
 
     // these variables create the element when used
     const divCreate1 = document.createElement('div');
@@ -70,7 +74,7 @@ function buildTasks(currentTask) {
     // based on the priority input for the task.
     PriorityCard.children[0].appendChild(divCreate1);
     // gives the created div the card class
-    divCreate1.setAttribute('class', 'card');
+    divCreate1.setAttribute('class', 'card m-3');
     divCreate1.setAttribute('data-id', currentTask.id);
     // creates another child div 
     // gives the class 'card body'
@@ -88,19 +92,21 @@ function buildTasks(currentTask) {
     divCreate2.appendChild(paragraphCreate);
     paragraphCreate.setAttribute('class', 'card-text');
 
+    // this is another button created undernead the card-body div
+    // it only has the class of button, but the plan is to make it an edit
+    // button using a modal if there is time.
+    divCreate2.appendChild(buttonCreate2);
+    buttonCreate2.setAttribute('class', 'btn btn-primary send-modal');
+    buttonCreate2.setAttribute('data-bs-toggle', 'modal');
+    buttonCreate2.setAttribute('data-bs-target', '#modalTemplate');
+    buttonCreate2.textContent = 'Edit';
+
     // creates a button underneath the card-body div,
     // this button has the classes btn-close, and text-reset
     // this will eventually be made into the mark complete button
     // and will remove the task from the page
     divCreate2.appendChild(buttonCreate1);
     buttonCreate1.setAttribute('class', `btn btn-close text-reset complete`);
-
-
-    // this is another button created undernead the card-body div
-    // it only has the class of button, but the plan is to make it an edit
-    // button using a modal if there is time.
-    divCreate2.appendChild(buttonCreate2);
-    buttonCreate2.setAttribute('class', 'btn')
 
     // this sets the header and the description to have the same text,
     // as the input entered into the object.
@@ -118,7 +124,10 @@ function completeTaskListener() {
 
     completeButton.forEach(button => {
 
-        button.addEventListener('click', event => markComplete(event)
+        button.addEventListener('click', event => {
+
+            (markComplete(event))
+        }
 
         )
     });
@@ -129,26 +138,22 @@ function markComplete(event) {
     currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'))
     // grab the nearest card class by the button interacted wtih
     const task = event.currentTarget.closest('.card');
-    
+
     // when the element exists, remove the task from the array
     if (task) {
         // grab the unique data id from the  targeted task
         const taskID = task.getAttribute('data-id');
-        
+
         // create an empty array to push the objects we want remaining into
         const updatedTasks = [];
         // for loop, iterate over each object in the array, if the unique id does not 
         // match with the targeted id, we want to keep it and push it into our updated
         // array, the only one left out should be the targeted object.
-  
+
 
         for (let i = 0; i < currentTasks.length; i++) {
             if (currentTasks[i].id !== parseInt(taskID)) {
-                console.log(parseInt(taskID));
-                console.log(currentTasks[i].id);
-                
                 updatedTasks.push(currentTasks[i]);
-                
             }
         }
         // currentTasks.id is a number and taskID is a string
@@ -164,6 +169,84 @@ function markComplete(event) {
     }
 }
 
+
+function updateTaskListener() {
+    let editButton = document.querySelectorAll(`.btn.btn-primary.send-modal`);
+
+    editButton.forEach(editButton => {
+
+        editButton.addEventListener('click', event => { editTask(event) })
+    });
+}
+
+function editTask(event) {
+
+    currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'));
+
+    const task = event.currentTarget.closest('.card');
+
+    if (task) {
+
+        const taskID = task.getAttribute('data-id');
+
+        for (let i = 0; i < currentTasks.length; i++) {
+            if (currentTasks[i].id === parseInt(taskID)) {
+
+                const taskName = currentTasks[i].taskName;
+                const description = currentTasks[i].description;
+                const priority = currentTasks[i].priority;
+
+                const currentTaskName = document.getElementById('currentTaskName');
+                const currentTaskDescription = document.getElementById('currentTaskDescription');
+                const currentTaskID = document.getElementById('currentTaskID');
+
+                currentTaskName.textContent = taskName;
+                currentTaskDescription.textContent = description;
+                currentTaskID.textContent = taskID;
+
+                console.log(currentTaskName);
+                console.log(currentTaskDescription);
+
+            }
+        }
+    }
+}
+
+modalSave.addEventListener('click', event => { saveTask(event) })
+
+function saveTask(event) {
+
+    currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'));
+
+    const updatedTasks = [];
+
+    const currentTaskID = document.getElementById('currentTaskID').textContent;
+    const currentTaskName = document.getElementById('currentTaskName').textContent;
+    const currentTaskPriorityInput = document.getElementById('currentTaskPriority_input').value;
+    const updatedTaskDescription = document.getElementById('updatedTaskDescription').value;
+
+    console.log(currentTaskID);
+    console.log(currentTaskName);
+
+    const editedTask = new TaskEntry(currentTaskName, updatedTaskDescription, currentTaskPriorityInput, parseInt(currentTaskID));
+
+    for (let i = 0; i < currentTasks.length; i++) {
+        if (currentTasks[i].id !== parseInt(currentTaskID)) {
+            updatedTasks.push(currentTasks[i]);
+        }
+    }
+
+    updatedTasks.push(editedTask);
+
+    localStorage.setItem('TaskSubmissions', JSON.stringify(updatedTasks));
+
+    location.reload();
+
+}
+
+
+
+
 function sortTasks() {
     currentTasks = JSON.parse(localStorage.getItem('TaskSubmissions'))
 
@@ -171,10 +254,11 @@ function sortTasks() {
     // currentTasks array. It will sort and build out each card for its 
     // corresponding task.
     for (let i = 0; i < currentTasks.length; i++) {
-        buildTasks(currentTasks[i], i);
+        buildTasks(currentTasks[i]);
     }
 
     completeTaskListener();
+    updateTaskListener()
 }
 
 
@@ -182,5 +266,6 @@ function sortTasks() {
 // when the page finishes loading, this calls the sortTasks fuction and builds
 // the cards for each task.
 window.addEventListener("load", sortTasks(currentTasks));
+
 
 
